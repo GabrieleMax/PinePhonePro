a0d4f5fb018f358c115d3b7f7dd2250111a5b0e6
GabrieleMax <gabriele1@hotmail.com>
2024-12-26 17:53:37 +0100

    Modified script

--- autoconfig.sh
+++ autoconfig.sh
@@ -4,11 +4,12 @@
 
 FIRSTUSER=$(grep "1000" /etc/passwd | awk -F ':' '{print $1}')                                                                
 # Url of the website with image links
-url="https://images.mobian-project.org/pinephonepro/installer/weekly/"
+deb_testing_url="https://images.mobian-project.org/pinephonepro/installer/weekly/"
 arch_url="https://github.com/dreemurrs-embedded/Pine64-Arch/releases/"
+kali_nethunter_url="https://kali.download/nethunterpro-images/kali-2024.4/"
 
 # Show the initial message
-echo "Connect the Pine Phone pro and press vol+ untill you'll see the blu led:"
+echo "Connect the Pine Phone pro (press vol+ untill the led will be blue):"
 
 # Save the initial output of lsblk, excluding partitions
 initial_devices=$(lsblk -dn -o NAME | sort)
@@ -36,16 +37,19 @@ done
 echo "The connected device is: /dev/$device_name"
 
 # Recover the list of files from the website
-deb_testing_posh=$(wget -q -O - "$url" | grep -oP 'mobian-installer-rockchip-phosh-\d{8}.img.xz' | sort -r | head -n 1)
-deb_testing_plasma=$(wget -q -O - "$url" | grep -oP 'mobian-installer-rockchip-plasma-mobile-\d{8}.img.xz' | sort -r | head -n 1)
+deb_testing_posh=$(wget -q -O - "$deb_testing_url" | grep -oP 'mobian-installer-rockchip-phosh-\d{8}.img.xz' | sort -r | head -n 1)
+deb_testing_plasma=$(wget -q -O - "$deb_testing_url" | grep -oP 'mobian-installer-rockchip-plasma-mobile-\d{8}.img.xz' | sort -r | head -n 1)
 arch_testing_posh=$(wget -q -O - "$arch_url" | grep -oP 'archlinux-pinephone-pro-phosh-\d{8}.img.xz' | sort -r | head -n 1)
+kali_nethunter=$(wget -q -O - "$kali_nethunter_url" | grep -oP 'kali-nethunterpro-\d{8}.img.xz' | sort -r | head -n 1)
 
 # Function to download the Mobian image with Phosh for PinePhone Pro
 deb_img_testing_posh() {
     if [ -n "$deb_testing_posh" ]; then
         echo "File found: $deb_testing_posh"
-        wget --progress=dot "$url$deb_testing_posh" -O "/tmp/image.xz"
+        wget --progress=dot "$deb_testing_url$deb_testing_posh" -O "/tmp/image.xz"
         echo "Download finished: $deb_testing_posh"
+        img_burn  # Automatically call the burn function after download
+        exit  # Exit after burn
     else
         echo "File not found."
     fi
@@ -55,8 +59,10 @@ deb_img_testing_posh() {
 deb_img_testing_plasma() {
     if [ -n "$deb_testing_plasma" ]; then
         echo "Most recent file found: $deb_testing_plasma"
-        wget --progress=dot "$url$deb_testing_plasma" -O "/tmp/image.xz"
+        wget --progress=dot "$deb_testing_url$deb_testing_plasma" -O "/tmp/image.xz"
         echo "Download finished: $deb_testing_plasma"
+        img_burn  # Automatically call the burn function after download
+        exit  # Exit after burn
     else
         echo "File not found."
     fi
@@ -64,16 +70,28 @@ deb_img_testing_plasma() {
 
 # Function to download the latest Arch image for PinePhone Pro
 arch_img_testing_posh() {
-    # Get the latest Arch PinePhone Pro image with Phosh
-    arch_testing=$(wget -q -O - "$arch_url" | grep -oP 'archlinux-pinephone-pro-phosh-\d{8}.img.xz' | sort -r | head -n 1)
-
-    if [ -n "$arch_testing" ]; then
-        echo "Most recent Arch image found: $arch_testing"
-        wget --progress=dot "$arch_url$arch_testing" -O "/tmp/image.xz"
-        echo "Download finished: $arch_testing"
+    if [ -n "$arch_testing_posh" ]; then
+        echo "Most recent Arch image found: $arch_testing_posh"
+        wget --progress=dot "$arch_url$arch_testing_posh" -O "/tmp/image.xz"
+        echo "Download finished: $arch_testing_posh"
+        img_burn  # Automatically call the burn function after download
+        exit  # Exit after burn
     else
         echo "Arch image not found."
     fi
+}
+
+# Function to download the latest Kali Linux Nethunter image for PinePhone Pro
+kali_nethunter_posh_img() {
+    if [ -n "$kali_nethunter" ]; then
+        echo "Most recent Kali Nethunter image found: $kali_nethunter"
+        wget --progress=dot "$kali_nethunter_url$kali_nethunter" -O "/tmp/image.xz"
+        echo "Download finished: $kali_nethunter"
+        img_burn  # Automatically call the burn function after download
+        exit  # Exit after burn
+    else
+        echo "Kali Nethunter image not found."
+    fi  
 }
 
 # Function for the image burn process
@@ -94,10 +112,11 @@ img_burn() {
 }
 
 # Menu with correct options
-PS3="Choose an option (1-4): "
+PS3="Choose an option (1-5): "
 select menu in "Download and install Debian testing with Plasma mobile" \
                "Download and install Debian testing with Phosh mobile" \
-               "Download and install Arch Linux with Phosh" \
+               "Download and install Arch Linux with Phosh mobile" \
+               "Download and install Kali Nethunter Linux with Phosh mobile" \
                "Image burn" \
                "Exit"; do
     case $menu in
@@ -110,8 +129,12 @@ select menu in "Download and install Debian testing with Plasma mobile" \
         "Download and install Arch Linux with Phosh")
             arch_img_testing_posh
             ;;
-        "Image burn")
+         "Download and install Kali Nethunter Linux with Phosh")
+            kali_nethunter_posh_img
+            ;;
+          "Image burn")
             img_burn
+            exit  # Exit after burn
             ;;
         "Exit")
             echo "Exiting the script."
